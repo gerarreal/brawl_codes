f37487f27afa28babea401534b8ce09acf4dc89c
Gerasimos <79531765+gerarreal@users.noreply.github.com>
2024-01-29 21:23:19 +0200

    Brief update (didn't like it)
    I was looking at the code and realized it doesn't work on Pokemon Stadium, props to Kapedani and MarioDox

--- ResultName.asm
+++ ResultName.asm
@@ -1,20 +1,17 @@
-########################################
-Expanded Result Screen Names [GeraRReal]
-# Minor update from the CBM release that just changes how jumps is done
-# Also added set related functions
-########################################
+###############################################################
+Expanded Result Screen Names + Pokemon Stadium Name [GeraRReal]
+###############################################################
 # <ID> corresponds to the Cosmetic ID in BrawlEx's folder
-# Set-specific Result Name, only difference is that it doesn't check for modulo 10.
-# You might wanna use this if you have a set of costumes for 1X or if your build skips the modulo 10 check with costumes and ect files.
-.macro setCostumeResultsName_set_nm(<ID>,<set>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>)
+
+# Range specific Result Name
+# Replaces set specific costumes to add more choice
+.macro replace_(<ID>,<min>,<max>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>,<register>)
 {
     cmpwi r5, <ID>		# Compare character
-    bne 0x48			# If it doesn't match, skip to the end
-	li r15, <set>		# Get set (must be 00,10,20,30,40,50)
-	cmpw r14, r15		# Compare with set ID
-	blt 0x3C			# If less, skip the check
-	addi r15, r15, 10	# Add 10
-	cmpw r14, r15		# Compare with set ID again
+    bne 0x40			# If it doesn't match, skip to the end
+	cmpwi r14, <min>	# Compare with min
+	blt 0x38			# If less, skip the check
+	cmpwi r14, <max>	# Compare with max
 	bge 0x30			# If greater and equal, skip the check
 	bl the_label		# Else, bl trick to the end and get the name
 	word <A>
@@ -26,38 +23,12 @@ Expanded Result Screen Names [GeraRReal]
 	word <G>
 	word <H>
 the_label:
-	mflr r5				# Get new character name
+	mflr <register>		# Get new character name
 	b %END%				# End
 }
-# Set-specific Result Name.
-.macro setCostumeResultsName_set(<ID>,<set>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>)
-{
-    cmpwi r5, <ID>		# Compare character
-    bne 0x54			# If it doesn't match, skip to the end
-	li r15, <set>		# Get set (must be 00,10,20,30,40,50)
-	cmpw r14, r15		# Compare with set ID
-	blt 0x48			# If less, skip the check
-	addi r15, r15, 10	# Add 10
-	cmpwi r15, 10		# Is it 10
-	bne 0x8				# Else skip next command
-	addi r15, r15, 10	# Add another 10
-	cmpw r14, r15		# Compare with set ID again
-	bge 0x30			# If greater and equal, skip the check
-	bl the_label		# Else, bl trick to the end and get the name
-	word <A>
-	word <B>
-	word <C>
-	word <D>
-	word <E>
-	word <F>
-	word <G>
-	word <H>
-the_label:
-	mflr r5				# Get new character name
-	b %END%				# End
-}
+
 # Costume-specific Result Name.
-.macro setCostumeResultsName(<ID>,<Costume>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>)
+.macro replace(<ID>,<Costume>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>,<register>)
 {
     cmpwi r5, <ID>		# Compare character
     bne 0x38			# If it doesn't match, skip to the end
@@ -73,42 +44,55 @@ the_label:
 	word <G>
 	word <H>
 the_label:
-	mflr r5				# Get new character name
+	mflr <register>		# Get new character name
 	b %END%				# End
 }
 
-# Costume-specific Result Name.
-.macro setCostumeResultsName(<ID>,<Costume>,<A>,<B>,<C>,<D>,<E>,<F>,<G>,<H>)
+HOOK @ $800AFA98
 {
-    cmpwi r5, <ID>		# Compare character
-    bne end				# If it doesn't match, skip to the end
-    cmpwi r14, <Costume># Check if costume ID
-    bne end				# If not equal, skip the check
-	bl the_label		# Else, bl trick to the end and get the name
-	word <A>
-	word <B>
-	word <C>
-	word <D>
-	word <E>
-	word <F>
-	word <G>
-	word <H>
-the_label:
-	mflr r5				# Get new character name
-	b %END%				# End
-end:
+    lbz r14, 0x26(r31)
+	stw r31, 0xC(r1)
 }
-HOOK @ $800e7c48
+
+HOOK @ $800AFA44
 {
-    lbz r14, 0x26(r3) 	# Get costume ID
-    lbz r3, 0x24(r3)  	# Original Function
+	lwz r3, -0x4340(r13)
+	lwz r14, 0x0018(r3)
+	cmpwi r24, 3
+	bgt leader
+	cmpwi r24, 0
+	blt leader
+	b normal
+normal:
+	mulli r15, r24, 0x2AC
+	b b_end
+leader:
+	mulli r15, r29, 0x2AC
+b_end:
+	add r14, r14, r15
+	mr r5, r31
+	lbz r14, 0x26(r14)
+	b %END%
 }
-#
+
+# Results Screen
 HOOK @ $800E7C54
 {
+	# Dr. Mario
+    %replace_(0x00,20,30,0x44722E20,0x4D617269,0x6F000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,r5)
 	# Dark Pit
-    %setCostumeResultsName_set(0x16,20,0x4461726B,0x20506974,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000)
+    %replace_(0x16,20,30,0x4461726B,0x20506974,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,r5)
     mr r5, r3           # Original Function
 	li r14, 0			# Return r14 to it's original value
 }
 
+# Pokemon Stadium
+HOOK @ $800AFA74
+{
+	# Dr. Mario
+    %replace_(0x00,20,30,0x44722E20,0x4D617269,0x6F000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,r3)
+	# Dark Pit
+    %replace_(0x16,20,30,0x4461726B,0x20506974,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,0x00000000,r3)
+	lwzx r3, r3, r0 	# Original function
+	li r14, 0 			# Return r14 to it's original value
+}
