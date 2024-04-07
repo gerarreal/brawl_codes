44f98b94a516f05ce5707151a930f48e01c3b1db
Gerasimos <79531765+gerarreal@users.noreply.github.com>
2024-04-07 17:10:01 +0300

    Add files via upload

---
+++ MultiSpy.asm
@@ -0,0 +1,14 @@
+##############################################################
+Multiple Spy Costumes for Everyone v1.1 [GeraRReal, DukeItOut]
+##############################################################
+# Reserved for a later update
+CODE @ $8084CB4C
+{
+    cmpwi r31, 0xFF
+    bne- 0x14
+    nop 
+    nop 
+    nop 
+    nop 
+}
+op nop @ $80835C34
